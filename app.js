319aeba917bd0f85efbb713eb390584b0b152d34
Andrey Larikov <larikov174@gmail.com>
2021-12-09 10:53:50 +0500

    dev: add CORS

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  next();
+}); 
+
 app.use('/users', require('./routes/users'));
 app.use('/posts', require('./routes/posts'));
 
